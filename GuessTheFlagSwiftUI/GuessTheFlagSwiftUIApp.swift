3fc1a71e8919bc4f5f86dd5e01e82dd2fcecef7b
dehasuer <dehasuer97@gmail.com>
2022-11-02 17:26:49 +0300

    Initial Commit

---
+++ GuessTheFlagSwiftUI/GuessTheFlagSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagSwiftUIApp.swift
+//  GuessTheFlagSwiftUI
+//
+//  Created by Deha Süer on 2.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
